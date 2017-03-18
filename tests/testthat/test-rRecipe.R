9384944737669c50856c766337737bc503d4d5d5
Michal Burda <michal.burda@centrum.cz>
2017-03-18 07:30:12 +0100

    rRecipe() test

---
+++ tests/testthat/test-rRecipe.R
@@ -0,0 +1,8 @@
+test_that('rRecipe', {
+  r <- rRecipe(target='target.Rdata', script='script.R', depends=c('dep1', 'dep2'))
+  expect_true(is.recipe(r))
+  expect_equal(r$target, 'target.Rdata')
+  expect_equal(r$depends, c('script.R', 'dep1', 'dep2'))
+  expect_equal(r$build, '$(Rcode) \'source("script.R")\'')
+  expect_equal(r$clean, 'rm target.Rdata')
+})
