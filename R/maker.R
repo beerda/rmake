47bfb3ede94dce95909ec99ead884abfc4954d45
Michal Burda <michal.burda@centrum.cz>
2017-03-18 09:27:49 +0100

    Removed global maker options variable

--- R/maker.R
+++ R/maker.R
@@ -1,17 +1,1 @@
 #' @import assertthat
-
-
-.optionsFactory <- function() {
-  defaults <- list(
-    'R.command'='R --no-save --no-restore -quiet -e',
-    'rm.command'='rm'
-  )
-
-  list(
-    get=function(name) { defaults[[name]] },
-    set=function(name, value) { defaults[[name]] <<- value }
-  )
-}
-
-#' @export
-maker <- .optionsFactory()
