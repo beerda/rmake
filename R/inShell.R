b602399a6b58cfcb54691a4f5d2f046ebb5855bf
Michal Burda <michal.burda@centrum.cz>
2017-03-18 05:56:46 +0100

    rCmd() renamed to inShell()

--- R/inShell.R
+++ R/inShell.R
@@ -1,7 +1,7 @@
-rCmd <- function(...) {
+inShell <- function(...) {
   f <- as.list(parent.frame())
   exprs <- lapply(pryr::dots(...), function(e) {
-    substitute_q(e, f)
+    pryr::substitute_q(e, f)
   })
   paste0('R --no-save --no-restore --quiet -e \'', as.character(exprs), '\'')
 }
