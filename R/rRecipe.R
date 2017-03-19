4f1b3e2dde005a98f4b0161d43af9dcfd15cb9b6
Michal Burda <michal.burda@centrum.cz>
2017-03-19 10:35:47 +0100

    Added rRecipe() support of multiple targets

--- R/rRecipe.R
+++ R/rRecipe.R
@@ -1,11 +1,11 @@
 #' @export
 rRecipe <- function(target, script, depends=NULL) {
-  assert_that(is.string(target))
+  assert_that(is.character(target))
   assert_that(is.string(script))
   assert_that(is.null(depends) || is.character(depends))
 
   recipe(target=target,
          depends=c(script, depends),
          build=inShell(source(script)),
-         clean=paste0('$(RM) ', target))
+         clean=paste0('$(RM) ', paste0(target, collapse=' ')))
 }
--- tests/testthat/test-rRecipe.R
+++ tests/testthat/test-rRecipe.R
@@ -1,8 +1,19 @@
-test_that('rRecipe', {
+test_that('single target rRecipe', {
   r <- rRecipe(target='target.Rdata', script='script.R', depends=c('dep1', 'dep2'))
   expect_true(is.recipe(r))
   expect_equal(r$target, 'target.Rdata')
   expect_equal(r$depends, c('script.R', 'dep1', 'dep2'))
   expect_equal(r$build, 'echo \'source("script.R")\\n\' | $(R)')
   expect_equal(r$clean, '$(RM) target.Rdata')
+})
+test_that('multiple target rRecipe', {
+  r <- rRecipe(target=c('target.Rdata', 'target2.Rdata'),
+               script='script.R',
+               depends=c('dep1', 'dep2'))
+  expect_true(is.recipe(r))
+  expect_equal(r$target, c('target.Rdata', 'target2.Rdata'))
+  expect_equal(r$pattern, c('target%Rdata', 'target2%Rdata'))
+  expect_equal(r$depends, c('script.R', 'dep1', 'dep2'))
+  expect_equal(r$build, 'echo \'source("script.R")\\n\' | $(R)')
+  expect_equal(r$clean, '$(RM) target.Rdata target2.Rdata')
 })
