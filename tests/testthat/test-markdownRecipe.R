9432f84495d33baf7aac916b19308b77e28a4626
Michal Burda <michal.burda@centrum.cz>
2018-01-29 13:16:34 +0100

    Attempt to fix unit tests on windows platform

--- tests/testthat/test-markdownRecipe.R
+++ tests/testthat/test-markdownRecipe.R
@@ -6,12 +6,18 @@ test_that('single target markdownRecipe', {
   expect_equal(r$target, 'target.pdf')
   expect_equal(r$pattern, 'target.pdf')
   expect_equal(r$depends, c('script.Rmd', 'dep1', 'dep2'))
-  expect_equal(sanitizeCovr(r$build),
-               c('echo \'{\\n\'\\',
-                 '\'    params <- NULL\\n\'\\',
-                 '\'    rmarkdown::render("script.Rmd", output_format = "all", output_file = "target.pdf")\\n\'\\',
-                 '\'}\\n\' | $(R)'))
   expect_equal(r$clean, '$(RM) target.pdf')
+
+  if (.Platform$OS.type == 'unix') {
+    expect_equal(sanitizeCovr(r$build),
+                 c('echo \'{\\n\'\\',
+                   '\'    params <- NULL\\n\'\\',
+                   '\'    rmarkdown::render("script.Rmd", output_format = "all", output_file = "target.pdf")\\n\'\\',
+                   '\'}\\n\' | $(R)'))
+  } else {
+    expect_equal(r$build,
+                 c('(echo {&echo     params ^<- NULL&echo     rmarkdown::render^("script.Rmd", output_format = "all", output_file = "target.pdf"^)&echo }) | $(R)'))
+  }
 })
 
 test_that('multiple target markdownRecipe', {
@@ -22,10 +28,16 @@ test_that('multiple target markdownRecipe', {
   expect_equal(r$target, c('target.pdf', 'target.docx'))
   expect_equal(r$pattern, c('target%pdf', 'target%docx'))
   expect_equal(r$depends, c('script.Rmd', 'dep1', 'dep2'))
-  expect_equal(sanitizeCovr(r$build),
-               c('echo \'{\\n\'\\',
-                 '\'    params <- NULL\\n\'\\',
-                 '\'    rmarkdown::render("script.Rmd", output_format = "all", output_file = c("target.pdf", "target.docx"))\\n\'\\',
-                 '\'}\\n\' | $(R)'))
   expect_equal(r$clean, '$(RM) target.pdf target.docx')
+
+  if (.Platform$OS.type == 'unix') {
+    expect_equal(sanitizeCovr(r$build),
+                 c('echo \'{\\n\'\\',
+                   '\'    params <- NULL\\n\'\\',
+                   '\'    rmarkdown::render("script.Rmd", output_format = "all", output_file = c("target.pdf", "target.docx"))\\n\'\\',
+                   '\'}\\n\' | $(R)'))
+  } else {
+    expect_equal(r$build,
+                 c('(echo {&echo     params ^<- NULL&echo     rmarkdown::render^("script.Rmd", output_format = "all", output_file = "target.pdf", "target.docx"^)&echo }) | $(R)'))
+  }
 })
--- tests/testthat/test-rRecipe.R
+++ tests/testthat/test-rRecipe.R
@@ -5,12 +5,19 @@ test_that('single target rRecipe', {
   expect_true(is.recipe(r))
   expect_equal(r$target, 'target.Rdata')
   expect_equal(r$depends, c('script.R', 'dep1', 'dep2'))
-  expect_equal(sanitizeCovr(r$build),
-               c('echo \'{\\n\'\\',
-                 '\'    params <- NULL\\n\'\\',
-                 '\'    source("script.R")\\n\'\\',
-                 '\'}\\n\' | $(R)'))
   expect_equal(r$clean, '$(RM) target.Rdata')
+
+  if (.Platform$OS.type == 'unix') {
+    expect_equal(sanitizeCovr(r$build),
+                 c('echo \'{\\n\'\\',
+                   '\'    params <- NULL\\n\'\\',
+                   '\'    source("script.R")\\n\'\\',
+                   '\'}\\n\' | $(R)'))
+  } else {
+    expect_equal(r$build,
+                 c('(echo {&echo     params <- NULL&echo     source("script.R")&echo }) | $(R)'))
+
+  }
 })
 
 test_that('multiple target rRecipe', {
@@ -21,10 +28,16 @@ test_that('multiple target rRecipe', {
   expect_equal(r$target, c('target.Rdata', 'target2.Rdata'))
   expect_equal(r$pattern, c('target%Rdata', 'target2%Rdata'))
   expect_equal(r$depends, c('script.R', 'dep1', 'dep2'))
-  expect_equal(sanitizeCovr(r$build),
-               c('echo \'{\\n\'\\',
-                 '\'    params <- NULL\\n\'\\',
-                 '\'    source("script.R")\\n\'\\',
-                 '\'}\\n\' | $(R)'))
   expect_equal(r$clean, '$(RM) target.Rdata target2.Rdata')
+
+  if (.Platform$OS.type == 'unix') {
+    expect_equal(sanitizeCovr(r$build),
+                 c('echo \'{\\n\'\\',
+                   '\'    params <- NULL\\n\'\\',
+                   '\'    source("script.R")\\n\'\\',
+                   '\'}\\n\' | $(R)'))
+  } else {
+    expect_equal(r$build,
+                 c('(echo {&echo     params <- NULL&echo     source("script.R")&echo }) | $(R)'))
+  }
 })
