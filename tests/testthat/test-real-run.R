df640c80ae8a70d090db63c0ad28b3cbcd16cff2
Michal Burda <michal.burda@centrum.cz>
2018-01-31 08:17:59 +0100

    Simplified working with test files in test-real-run.R

--- tests/testthat/test-real-run.R
+++ tests/testthat/test-real-run.R
@@ -1,42 +1,24 @@
 initTesting <- function(name) {
-  theTestingName <<- name
-  theTestingFiles <<- NULL
-  setwd(tempdir())
+  theCounter <<- 1
+  theWorkingDir <<- tempfile(pattern=name)
+  dir.create(theWorkingDir)
+  setwd(theWorkingDir)
 }
 
-doneTesting <- function() {
-  file.remove(theTestingFiles, showWarnings=FALSE)
-  theTestingName <<- NULL
-  theTestingFiles <<- NULL
-}
-
-createDepFile <- function() {
-  f <- tempfile(pattern=theTestingName, fileext='.in')
+createDepFile <- function(f) {
   cat(paste0(as.character(as.numeric(Sys.time())), '\n'), file=f)
-  theTestingFiles <<- c(theTestingFiles, f)
-  return(f)
 }
 
-createScriptFile <- function(out) {
-  f <- tempfile(pattern=theTestingName, fileext='.R')
+createScriptFile <- function(f, out) {
   cat(paste0('cat(paste0(as.character(as.numeric(Sys.time())), "\\n"), file="', out, '")\n', collapse=''),
       file=f)
-  theTestingFiles <<- c(theTestingFiles, f)
-  return(f)
 }
 
 createMakefile <- function(...) {
   makerSkeleton()
   Sys.sleep(1)
   print(c(...))
-  cat(paste(..., sep='\n'), file=file.path(tempdir(), 'Makefile.R'))
-  theTestingFiles <<- c(theTestingFiles, file.path(tempdir(), c('Makefile.R', 'Makefile')))
-}
-
-getOutFileName <- function() {
-  o <- tempfile(pattern=theTestingName, fileext='.out')
-  theTestingFiles <<- c(theTestingFiles, o)
-  return(o)
+  cat(paste(..., sep='\n'), file='Makefile.R')
 }
 
 contentGreater <- function(f1, f2) {
@@ -51,10 +33,13 @@ runSystem <- function(cmd) {
 
 test_that('simple R script', {
   initTesting('simple')
-  dep1 <- createDepFile()
-  dep2 <- createDepFile()
-  out <- getOutFileName()
-  r <- createScriptFile(out)
+  dep1 <- 'dep1.in'
+  dep2 <- 'dep2.in'
+  out <- 'result.out'
+  r <- 'script.R'
+  createDepFile(dep1)
+  createDepFile(dep2)
+  createScriptFile(r, out)
   createMakefile('library(maker)',
                  paste0('job <- list(rRecipe("', out, '", "', r, '", c("', dep1, '", "', dep2, '")))'),
                  'makefile(job)')
@@ -70,5 +55,4 @@ test_that('simple R script', {
   expect_true(file.exists(out))
   expect_true(contentGreater(out, dep1))
   expect_true(contentGreater(out, dep2))
-  doneTesting()
 })
