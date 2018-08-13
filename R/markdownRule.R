b4c54ca31fc706c9ad38541e42d82f2d82e5e51c
Michal Burda <michal.burda@centrum.cz>
2018-08-13 08:38:27 +0200

    Fixed documentation typo

--- R/markdownRule.R
+++ R/markdownRule.R
@@ -4,7 +4,7 @@
 #' of various supported formats such as (PDF, DOCX, etc.).
 #'
 #' This rule executes the following command in a separate R process:
-#' `params <- params; rmarkdown::render(script, output_format=format, output_file=target)``
+#' `params <- params; rmarkdown::render(script, output_format=format, output_file=target)`
 #'
 #' That is, parameters given in the `params` argument are stored into the global variable
 #' and then the `script` is rendered with rmarkdown. That is, the re-generation of the
