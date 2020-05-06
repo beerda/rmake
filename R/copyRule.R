e7774fb86eadbcd8f7263c97c4b79b16ed53ff76
Michal Burda <michal.burda@centrum.cz>
2020-05-06 12:01:31 +0200

    Fixed window paths in copyRule()

--- R/copyRule.R
+++ R/copyRule.R
@@ -4,9 +4,14 @@ copyRule <- function(target, depends, task='all') {
   assert_that(is.character(depends))
   assert_that(is.character(task))
 
+  dep <- depends[1]
+  if (.Platform$OS.type != 'unix') {
+    dep <- gsub('/', '\\', dep, fixed=TRUE)
+  }
+
   rule(target=target,
        depends=depends,
-       build=paste0('$(CP) ', depends[1], ' ', target),
+       build=paste0('$(CP) ', dep, ' ', target),
        clean=paste0('$(RM) ', paste0(target, collapse=' ')),
        task=task,
        type='copy')
